3028fc01991f03447b2b5546707ae799306826bf
cuteharam <dlatjgus0503@naver.com>
2024-01-17 17:25:38 +0900

    2024.1.17 애니메이션 클립

---
+++ Lecture/Assets/second class/scripts/sphere.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class sphere : MonoBehaviour
+{
+    SphereCollider sphereCollider;
+    
+    void Start()
+    {
+        sphereCollider = GetComponent<SphereCollider>();
+      
+        sphereCollider.isTrigger = true;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
